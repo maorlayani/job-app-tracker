a95338411829d5fed0b113b40af54b5c0ab2ca0d
Maor Layani <maorlayani@gmail.com>
2023-03-09 12:54:44 +0200

    Add general styling definitions

---
+++ src/components/styles/helper.styled.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const FlexRowCenterContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+export const FlexColumnContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+` 
