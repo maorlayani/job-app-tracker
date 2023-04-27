e8ea37e3a8ff2a3c50328fc45dc56d311a700c0b
maorlayani <maorlayani@gmail.com>
2023-04-27 17:29:43 +0300

    add progress bar cmp

---
+++ src/components/progress-bar/interface-progress-bar.ts
@@ -0,0 +1,9 @@
+import { Application } from "../../models/interfaces";
+
+export interface ProgressBarProps {
+    application: Application,
+}
+
+export interface MarkProps {
+    left: string
+}
---
+++ src/components/progress-bar/progress-bar.tsx
@@ -0,0 +1,29 @@
+import { Status } from "../../models/enums"
+import { ProgressBarProps } from "./interface-progress-bar"
+import { InsideMark, Mark, MarkContainer, MarkSign, MarkTitle, StyledProgressBar } from "./styled-progress-bar"
+
+export const ProgressBar: React.FC<ProgressBarProps> = ({ application }) => {
+
+    const getStatusMarks = () => {
+        const status = Object.keys(Status)
+        return status
+    }
+
+    const getPosition = (pos: number, length: number): string => {
+        let left: number = pos > 0 ? ((96 * pos) / length) : pos
+        return `${left.toFixed(0)}%`
+    }
+
+    if (!application.status) return <></>
+    return <StyledProgressBar>
+        {getStatusMarks().map((s, idx, arr) =>
+            <MarkContainer>
+                {s === application.status.toLowerCase() && <MarkSign left={getPosition(idx, arr.length - 1)}></MarkSign>}
+                <Mark left={getPosition(idx, arr.length - 1)} key={s}>
+                    {s === application.status.toLowerCase() && <InsideMark></InsideMark>}
+                </Mark>
+                <MarkTitle left={getPosition(idx, arr.length - 1)}>{s}</MarkTitle>
+            </MarkContainer>)}
+
+    </StyledProgressBar >
+} 
