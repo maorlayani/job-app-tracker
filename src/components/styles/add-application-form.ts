2cf944deaa2316ac907b877406792dc9a217fb41
maorlayani <maorlayani@gmail.com>
2023-04-27 17:23:36 +0300

    delete old nav

--- src/components/styles/add-application-form.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components"
-
-export const StyledAddApplicationForm = styled.form`
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 30px;
-`
