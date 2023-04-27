a1eae3c4df473dd1e1d25d1b4b5ead79d83d3e06
maorlayani <maorlayani@gmail.com>
2023-04-27 16:13:34 +0300

    update config files and app.js/css files

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ import { Route, Routes } from 'react-router-dom'
 import { TrackerBoard } from './pages/tracker-board'
 import { AddApplication } from './components/add-application/add-application'
 import { AppHeader } from './components/app-header/app-header'
+import { ApplicationFullDetails } from './pages/application-full-details/application-full-details'
 
 const AppStyle = styled.div`
-  background-color: #f7ebff84;
   padding: 0;
   margin: 0;
 `
@@ -18,8 +18,11 @@ export const App = () => {
       <AppHeader />
       <Routes>
         <Route path='' element={<HomePage />} />
-        <Route path='tracker' element={<TrackerBoard />} />
+        <Route path='tracker' element={<TrackerBoard />} >
+          <Route path=':applicationId' element={<ApplicationFullDetails />} />
+        </Route>
         <Route path='/edit/:id?' element={<AddApplication />} />
+        {/* <Route path='/tracker/:id?' element={<ApplicationFullDetails />} /> */}
       </Routes>
     </AppStyle >
   )
--- src/private.ts
+++ src/private.ts
@@ -2,3 +2,5 @@
 // export const MY_BRAND_API_KEY = 'C34dtNh9CGSsCWT/ZoUHY38CI2zUHt6O2ZaJIvEQav8='
 export const MY_BRAND_API_KEY = 'lcD+lnF6KlaPBIOREjmIawecQGO6YzxvenSIW272pWw='
 export const MY_BRAND_BASE_URL = `https://api.brandfetch.io/v2/brands/`
+
+export const GOOGLE_MAPS_API_KEY = 'AIzaSyA7rvYA7t1oBrA5ElVxq0q8s4BB783QUUs'
