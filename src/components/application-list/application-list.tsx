b7e7829c508f8528c9cbbca9b8969c7ec42bd203
maorlayani <maorlayani@gmail.com>
2023-04-30 18:10:25 +0300

    add ul element styles in application-list cmp

--- src/components/application-list/application-list.tsx
+++ src/components/application-list/application-list.tsx
@@ -1,9 +1,29 @@
+import styled from "styled-components"
 import { useAppSelector } from "../../hooks/redux-hooks"
 import { RootState } from "../../store/store"
 import { ApplicationLoader } from "../application-loader/application-loader"
 import { ApplicationPreview } from "../application-preview/application-preview"
 import { ApplicationListStyle, StyledListTitle } from "./styled-application-list"
 
+
+const ListUl = styled.ul`
+    display: flex;
+    flex-wrap: wrap;
+    gap: 20px;
+    justify-content: center;
+    width: 500px;
+    margin-block-end: 20px;
+    @media (min-width: 800px) {
+        width: 750px;
+    }
+    @media (min-width: 1050px) {
+        width: 1000px;
+    }
+    @media (min-width: 1270px) {
+        justify-content: flex-start;
+        width: 1240px;
+    }
+`
 export const ApplicationList = () => {
 
     const applications = useAppSelector((state: RootState) => state.tracker.applications)
@@ -22,17 +42,17 @@ export const ApplicationList = () => {
         {isLoading && <ApplicationLoader />}
         {filteredApplicationByIsPinned(true)[0] && <>
             <StyledListTitle>Pinned Applications</StyledListTitle>
-            <ul>
+            <ListUl >
                 {filteredApplicationByIsPinned(true).map(app => {
                     return <ApplicationPreview key={app._id} application={app} />
                 })}
-            </ul>
+            </ListUl>
         </>}
         {filteredApplicationByIsPinned(true)[0] && <StyledListTitle>Other Applications</StyledListTitle>}
-        <ul>
+        <ListUl >
             {filteredApplicationByIsPinned(false).map(app => {
                 return <ApplicationPreview key={app._id} application={app} />
             })}
-        </ul>
+        </ListUl>
     </ApplicationListStyle>
 }
