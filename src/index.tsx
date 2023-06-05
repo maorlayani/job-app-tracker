c6c8769f5021e214c84688f26981b1dfcc4be5b9
maorlayani <maorlayani@gmail.com>
2023-06-05 13:06:32 +0300

    update app name

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import { App } from './App';
+import { App } from './app';
 import { HashRouter as Router } from 'react-router-dom'
 import * as serviceWorkerRegistration from './services/service-worker/serviceWorkerRegistration';
 import { Provider } from 'react-redux'
